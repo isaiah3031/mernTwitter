aba8f008107474bcdaf260b6189f663317cebc22
isaiah3031 <isaiah.whitfield@gmail.com>
2021-04-14 09:51:39 -0400

    Created the users Model.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
 
-router.get("/test", (req, res) => 
+router.get("/test", (req, res) =>
   res.json({ msg: "This is the users route" }));
+
+
+
 
 module.exports = router;
