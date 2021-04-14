cfac75c873aa7dca48e706ba974ea68ab93ddec8
isaiah3031 <isaiah.whitfield@gmail.com>
2021-04-14 09:41:20 -0400

    Added routes for user and tweets.

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+
 mongoose
-  .connect(db, { useUnifiedTopology: true })
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-app.get("/", (req, res) => res.send("Hello World"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World")
+});
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
